5fd6b8b4040bc4522250b03cdd24a5e01947773f
Akshar Goyal <claim.akshar@gmail.com>
2023-04-20 12:50:34 +0530

    Prepared models for transformation

--- models/production/toronto_covid_dbt.sql
+++ models/production/toronto_covid_dbt.sql
@@ -10,7 +10,7 @@
 
 
 with toronto_data as (
-    select * from {{ source('staging', 'ext_partitioned_clustered_covid_toronto_data') }} order by Episode_Date
+    select * from {{ source('production', 'ext_partitioned_clustered_covid_toronto_data') }} order by Episode_Date
 )
 select 
 
